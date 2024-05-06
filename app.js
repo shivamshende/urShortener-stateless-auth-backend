a7445ee4932a8c254ed39782ced0394c98e287f9
Shivam Shende <Shivamshende200@gmail.com>
2024-05-06 11:49:19 +0530

    refactor- udpate app.js

--- app.js
+++ app.js
@@ -12,12 +12,7 @@ connectDB()
 const app = express();
 const port = process.env.PORT || 4000;
 
-const corsOptions = {
-    origin: true,
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
